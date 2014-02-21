b69a7851082951679447051174bce526230b4031
timkg <tim.theodor@gmail.com>
2014-02-21 17:10:43 +0100

    added helper method which counts number of vocab items in course

--- app/scripts/models/course.js
+++ app/scripts/models/course.js
@@ -1,5 +1,12 @@
 var Course = Backbone.Model.extend({
 	initialize: function () {
 		// ...
+	},
+	getNumberOfVocabItems: function () {
+		var count = 0;
+		this.get('lessons').each(function (lesson) {
+			count += lesson.get('vocabItems').length;
+		})
+		return count;
 	}
 });
