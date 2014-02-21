9a46ab5aa33e2de7505905f37a925d38101ab0a0
timkg <tim.theodor@gmail.com>
2014-02-21 15:59:40 +0100

    removed random creation on initialize

--- app/scripts/vocabItemCollection.js
+++ app/scripts/vocabItemCollection.js
@@ -1,10 +1,6 @@
 var VocabItemCollection = Backbone.Collection.extend({
-	model: VocabItemModel,
-	initialize: function () {
-		var modelArray = [];
-		for (var i=0; i<20; i++) {
-			modelArray.push(new VocabItemModel());
-		}
-		this.set(modelArray);
+	model: VocabItem,
+	initialize: function (models, options) {
+		// ...
 	}
 });
